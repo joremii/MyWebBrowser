7d6e5cd84daa6bace1296f2124dfac8ce1a3e932
joremii <22022505@rp.edu.sg>
2023-08-18 13:21:37 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22022505/mywebbrowser/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id22022505.mywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoad;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoad = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
